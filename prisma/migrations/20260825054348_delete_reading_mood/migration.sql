fc1c53a18bfd8fb572c687b0d891f1d57913ece0
이성원 <57swji@gmail.com>
2026-08-25 14:58:58 +0900

    (reading-log): 🗃️ ReadingMood 제거 마이그레이션 추가
    앞선 스키마 변경(9faffd4)에 대응하는 마이그레이션.
    ALTER TABLE ReadingLog DROP COLUMN readingMood;

---
+++ prisma/migrations/20260825054348_delete_reading_mood/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `readingMood` on the `ReadingLog` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `ReadingLog` DROP COLUMN `readingMood`;
